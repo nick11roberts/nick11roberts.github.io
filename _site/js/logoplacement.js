34b6fe12439471702d4bb40da8e30d5f388c0dfb
nick11robers <nick.roberts.127.0.0.1@gmail.com>
2014-08-02 21:22:21 -0700

    Added some Jekyll stuff and modified iframe. Modded to single column layout for now.

---
+++ _site/js/logoplacement.js
@@ -0,0 +1,7 @@
+//var htmlWidth = $( document ).width();
+		
+function logoPlacement(){
+	var logoContainer = document.getElementById('logoContainer');
+
+	logoContainer.style.paddingLeft = '200px';
+} 
