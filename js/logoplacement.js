ce8e879765fae7d7e519ca464f7b2116f463fc79
nick11robers <nick.roberts.127.0.0.1@gmail.com>
2014-08-03 17:49:18 -0700

    Edited travis build file.

--- js/logoplacement.js
+++
@@ -1,7 +0,0 @@
-//var htmlWidth = $( document ).width();
-		
-function logoPlacement(){
-	var logoContainer = document.getElementById('logoContainer');
-
-	logoContainer.style.paddingLeft = '200px';
-} 
