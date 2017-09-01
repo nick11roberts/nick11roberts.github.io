7c480afdfb1c603a64b02d4d777b5927312f4f1c
Nicholas Carl Roberts <nick11roberts@users.noreply.github.com>
2017-08-31 21:39:32 -0700

    Upgrade to new site (4.0?!?)

--- javascript/resume.js
+++
@@ -1,8 +0,0 @@
-// Only executed our code once the DOM is ready.
-window.onload = function() {
-
-   // For canvas polygon bg
-   fillCanvas(document.getElementById('background'),
-      '#E2E0DE', 26, 13, 4);
-
-}
